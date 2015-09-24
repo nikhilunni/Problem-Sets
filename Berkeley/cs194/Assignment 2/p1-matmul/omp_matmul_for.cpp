c2bffa3e475bad07922fd463789c46d50a875942
Nikhil_Unni <nikhilunni@berkeley.edu>
2015-09-23 19:03:31 -0700

    intial cs194/assignment2 done

--- Berkeley/cs194/Assignment 2/p1-matmul/omp_matmul_for.cpp
+++ Berkeley/cs194/Assignment 2/p1-matmul/omp_matmul_for.cpp
@@ -8,6 +8,7 @@ void omp_for_matmuld(double **a, double **b, double **c, int nthr)
 
   /* CS194: add pragmas to this loop-nest
    * to enable OpenMP for parallelism */
+  #pragma omp parallel for collapse(3)
   for(int i=0;i<1024;i++)
     for(int j=0;j<1024;j++)
       for(int k=0;k<1024;k++)
